2a9d5d9dcfaf5f692e56b169ddc93c91b4789678
aula7 <pc704@gmail.com>
2017-03-21 13:29:43 -0500

    configuro toolbar e ir a una activity

--- app/src/main/java/com/example/aula7/primerparcial/ProfileActivity.java
+++ app/src/main/java/com/example/aula7/primerparcial/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.example.aula7.primerparcial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+        public void onConfigToolbar (){
+            Toolbar toolbar =(Toolbar) findViewById(R.id.Toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Volver a la Lista");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
